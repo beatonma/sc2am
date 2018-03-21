0f7926c07ebe2d0478a4073b487afe1a0abb663e
Michael Beaton <beatonma@gmail.com>
2018-03-21 13:30:12 +0000

    Removed unnecessary logging

--- web/translation/index.js
+++ web/translation/index.js
@@ -2,7 +2,6 @@ const i18n = require('i18n');
 
 function translate(text, locale) {
     i18n.setLocale(locale || 'en_GB');
-    console.log('Translating with locale=' + i18n.getLocale());
     const r = /_\((.*?)\)/g;
     let m;
     while (m = r.exec(text)) {
