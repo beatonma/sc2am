3e0117d5523fa34e57915b1e32da6cd9da1051d1
Michael Beaton <beatonma@gmail.com>
2018-03-21 13:24:15 +0000

    Updated templates

--- web/templates/profile/index.js
+++ web/templates/profile/index.js
@@ -13,61 +13,10 @@ function getTemplate(name) {
     return fs.readFileSync(path.join(__dirname, name), 'utf-8');
 }
 
-function buildProfile(profile) {
-    const htmlHead = buildHtmlHead(profile);
-    const htmlHeader = buildHtmlHeader(profile.achievements.points);
-    const htmlAchievements = buildHtmlAchievements(profile.achievements.achievements);
-
-    return getTemplate('base.html').formatUnicorn({
-        head: htmlHead,
-        header: htmlHeader,
-        achievements: htmlAchievements,
-    }).formatUnicorn({
-        username: profile.displayName
-    });
-}
-
-function buildHtmlHead(profile) {
-    return getTemplate('head.html').formatUnicorn({
-        achievements: JSON.stringify(profile.achievements.achievements),
-    });
-}
-
-function buildHtmlHeader(points) {
-    const template = getTemplate('header_points.html');
-    const categoryPoints = points.categoryPoints;
-    let html = '';
-    for (let id in categoryPoints) {
-        html += template.formatUnicorn({
-            category: lookup(id),
-            points: categoryPoints[id],
-        })
-    }
-    return getTemplate('header.html').formatUnicorn({
-        total: points.totalPoints,
-        points: html,
-    });
-}
-
-function buildHtmlAchievements(achievements) {
-    const template = getTemplate('achievement.html');
-
-    let html = '';
-    for (let a in achievements) {
-        html += template;
-    //     const ach = achievements[a];
-    }
-    return html;
+function buildProfileStub(user) {
+    return getTemplate('base.html').formatUnicorn(user);
 }
 
 module.exports = {
-    // base: base,
-    // head: head,
-    // header
-    // 'base': fs.readFileSync(path.join(__dirname, './base.html'), 'utf-8'),
-    // 'head': fs.readFileSync(path.join(__dirname, './head.html'), 'utf-8'),
-    // 'header': fs.readFileSync(path.join(__dirname, './header.html'), 'utf-8'),
-    // 'header_points': fs.readFileSync(path.join(__dirname, './header_points.html'), 'utf-8'),
-    // 'achievement': fs.readFileSync(path.join(__dirname, './achievement.html'), 'utf-8'),
-    'build': buildProfile,
+    'stub': buildProfileStub,
 }
