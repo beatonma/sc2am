1ad84b0669d14ebb68bb022ba29efe9777e1b752
Michael Beaton <beatonma@gmail.com>
2018-03-24 23:29:50 +0000

    Added sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@ const gulp = require('gulp');
 const minifyjs = require('gulp-babel-minify');
 const minifycss = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
-gulp.task('default', ['minifyjs', 'minifycss', 'watch']);
+gulp.task('default', ['sass', 'minifyjs', 'minifycss', 'watch']);
 
 gulp.task('watch', () => {
+    gulp.watch('web/assets/scss/**/*.scss', ['sass']);
     gulp.watch('web/assets/**/*.js', ['minifyjs']);
     gulp.watch('web/assets/**/*.css', ['minifycss']);
 });
 
 gulp.task('minifyjs', () => {
     return gulp.src(['web/assets/**/*.js', '!web/assets/**/*.min.js'])
-        .pipe(minifyjs())
+        .pipe(minifyjs().on('error', logError))
         .pipe(rename(path => {
             path.extname = '.min.js';
         }))
@@ -21,9 +23,20 @@ gulp.task('minifyjs', () => {
 
 gulp.task('minifycss', () => {
     return gulp.src(['web/assets/**/*.css', '!web/assets/**/*.min.css'])
-        .pipe(minifycss())
+        .pipe(minifycss().on('error', logError))
         .pipe(rename(path => {
             path.extname = '.min.css';
         }))
         .pipe(gulp.dest('web/assets'))
 });
+
+gulp.task('sass', () => {
+    return gulp.src('web/assets/scss/**/*.scss')
+        .pipe(sass().on('error', logError))
+        .pipe(gulp.dest('web/assets/css'));
+});
+
+function logError(err) {
+    console.error(err);
+    this.emit('end');
+}
