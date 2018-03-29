3820d24cfee3b56b26c82f0bfbd5cd9ad2c63850
Michael Beaton <beatonma@gmail.com>
2018-03-29 23:20:21 +0100

    Config can now be read from environment variables (for heroku)

--- config/index.js
+++ config/index.js
@@ -1,1 +1,18 @@
-module.exports = require('./config.json');
+// module.exports = require('./config.json');
+const fs = require('fs');
+const path = require('path');
+
+try {
+    const conf = fs.readFileSync(
+        path.join(__dirname, './config.json'),
+        'utf-8');
+    Object.assign(module.exports, JSON.parse(conf));
+}
+catch (err) {
+    Object.assign(module.exports, {
+        api_key: process.env.BATTLENET_KEY,
+        cache_refresh_rate: process.env.CACHE_REFRESH_RATE || 86400,
+        debug: process.env.DEBUG || false,
+        port: process.env.PORT || 9876,
+    });
+}
