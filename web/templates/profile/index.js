07c5f70c60ef0832e958ba8940926396289af444
Michael Beaton <beatonma@gmail.com>
2018-03-29 15:48:16 +0100

    Updated template formatting function

--- web/templates/profile/index.js
+++ web/templates/profile/index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// const lookup = require('../../bnet').api.lookup;
-
 const base = fs.readFileSync(path.join(__dirname, './base.html'), 'utf-8');
 
 function getTemplate(name) {
@@ -10,7 +8,24 @@ function getTemplate(name) {
 }
 
 function buildProfileStub(user) {
-    return getTemplate('base.html').formatUnicorn(user);
+    return format(getTemplate('base.html'), user);
+}
+
+function format() {
+    if (arguments && arguments.length > 1) {
+        let template = arguments[0];
+
+        const t = typeof arguments[1];
+        const args =
+            ("string" === t || "number" === t)
+                ? Array.prototype.slice.call(arguments)
+                : arguments[1];
+
+        for (let key in args) {
+            template = template.replace(new RegExp('\\{' + key + '\\}', 'gi'), args[key]);
+        }
+        return template;
+    }
 }
 
 module.exports = {
